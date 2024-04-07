185d74195e11b6a845aec058d9640a990750ccb6
jjiiiwooo <jiuuchoe@gmail.com>
2024-04-07 15:37:55 +0900

    Item css 수정

--- src/component/RestaruantItem.js
+++ src/component/RestaruantItem.js
@@ -2,32 +2,50 @@ import styled from "styled-components";
 import { FaStar } from "react-icons/fa";
 
 const Container = styled.div`
+  box-sizing: content-box;
+  border-radius: 30px;
   place-items: center;
-  border-top: 1px solid #dee2e6;
+  border: 1px solid #dee2e6;
+
+  & img {
+    border-radius: 30px;
+    object-fit: scale-down;
+  }
+
+  .name {
+    font-weight: bold;
+    padding-left: 2vw;
+    margin-bottom: -1vw;
+  }
+
+  .box {
+    display: flex;
+    margin-left: 2vw;
+
+    .review {
+      padding-left: 2vw;
+    }
+  }
+
   //컴포넌트간 간격
   & {
     border-top: 1px solid #dee2e6;
-    margin-left: 2vh;
+    margin: 2vh;
   }
 `;
-
-const Image = styled.img`
-  border-radius: 30px;
-  object-fit: scale-down;
-`;
-
-const Name = styled.div``;
 
 function RestaruantItem({ name, img, review, star }) {
   return (
     <Container>
-      <Image src={img} alt={name} />
-      <Name>{name}</Name>
-      <p>(리뷰:{review})</p>
-      <p>
-        <FaStar color="yellow" />
-        {star}
-      </p>
+      <img src={img} alt={name} />
+      <p className="name">{name}</p>
+      <div className="box">
+        <p className="star">
+          <FaStar color="yellow" />
+          {star}
+        </p>
+        <p className="review">(리뷰:{review})</p>
+      </div>
     </Container>
   );
 }
